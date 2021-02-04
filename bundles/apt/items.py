ead5bc22bdf19536eef3a737d164f5e07940a643
Zellyn Hunter <zellyn@squareup.com>
2021-02-04 10:12:46 -0500

    apt

--- bundles/apt/items.py
+++ bundles/apt/items.py
@@ -1,6 +1,51 @@
+# Copied from https://git.sr.ht/~bfiedler/bundlewrap/tree/main/item/bundles/apt
+
+pkg_apt = {}
+
 actions = {
-    'update': {
-        'command': 'sudo apt-get update && sudo apt-get -y upgrade',
-        'interactive': True,
+    'apt-update': {
+        'command': 'apt-get update',
+        'needed_by': {'pkg_apt:'},
+        'triggered': True,
+        'cascade_skip': False,
     }
 }
+
+files = {
+    # '/etc/apt/sources.list': {
+    #     'triggers': {'action:apt-update'},
+    # },
+
+    # '/etc/apt/preferences.d/releases.pref': {},
+    # '/etc/apt/preferences.d/packages.pref': {
+    #     'content_type': 'mako',
+    # },
+
+    '/etc/apt/apt.conf.d/90recommended-packages': {}
+}
+
+if node.metadata.get('apt/autoupgrade', True):
+    pkg_apt['unattended-upgrades'] = {}
+    files['/etc/apt/apt.conf.d/10periodic'] = {}
+
+directories = {
+    '/etc/apt/sources.list.d': {
+        # TODO: enable purging once all files are under bw's control
+        'purge': node.metadata.get('apt/managed-by-bw', False),
+    },
+    '/etc/apt/preferences.d': {
+        # TODO: enable purging once all files are under bw's control
+        'purge': node.metadata.get('apt/managed-by-bw', False),
+    },
+}
+
+for name, repos in node.metadata.get('apt/repos', {}).items():
+    files['/etc/apt/sources.list.d/{}.list'.format(name)] = {
+        'content': '\n'.join(repos)+'\n',
+        'triggers': {
+            'action:apt-update',
+        },
+    }
+
+for package, options in node.metadata.get('apt/packages', {}).items():
+    pkg_apt[package] = options
--- nodes.py
+++ nodes.py
@@ -1,14 +1,20 @@
 nodes = {
     'katespi1': {
-        'hostname': "ubuntu@100.89.32.19",
+        'hostname': 'ubuntu@100.89.32.19',
     },
     'katespi2': {
-        'hostname': "ubuntu@100.113.249.70",
+        'hostname': 'ubuntu@100.113.249.70',
     },
     'katespi3': {
-        'hostname': "ubuntu@100.88.221.38",
+        'hostname': 'ubuntu@100.88.221.38',
     },
     'katespi4': {
-        'hostname': "ubuntu@100.109.4.116",
+        'hostname': 'ubuntu@100.109.4.116',
+    },
+    'pizero': {
+        'hostname': 'pi@100.78.213.45',
+    },
+    'casepi': {
+        'hostname': 'pi@100.79.85.31',
     },
 }
